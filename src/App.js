9bb0fbd0347be30935da291530ea5d20e388fef0
Dean Bolte <dean.bolte7@gmail.com>
2022-07-31 16:25:24 +1000

    added boilerplate code and react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom"
+
+import Landing from './components/Landing';
+import Forum from './components/Forum';
+import Settings from './components/Settings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path={"/"} component={Landing} />
+          <Route exact path={"/forum"} component={Forum} />
+          <Route exact path={"/settings"} component={Settings} />
+        </Switch>
+    </Router>
   );
 }
 
