8c42b427b195daa73b55a55fe8537fe8a3be2466
Dean Bolte <dean.bolte7@gmail.com>
2022-08-22 20:44:37 +1000

    added some layout design and button css to settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div className='settings-page'>
-      <h1 className='logo'>Settings</h1>
+    <div>
+      <div className='split split-tiny left'>
+        <p className='settings-menu'>Menu</p>
+        <p className='settings-button'>User Settings</p>
+        <p className='settings-button'>App Settings</p>
+      </div>
+      <div className='split split-huge right'>
+        <h1 className='logo'>Settings</h1>
+        
+      </div>
+      
     </div>
   )
 }
