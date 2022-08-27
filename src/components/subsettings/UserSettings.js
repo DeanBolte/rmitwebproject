0869dc84df60474237869ab0816b26b0be8b3ff7
Dean Bolte <dean.bolte7@gmail.com>
2022-08-27 21:06:45 +1000

    added modifyable entries in user settings

--- src/components/subsettings/UserSettings.js
+++ src/components/subsettings/UserSettings.js
@@ -10,18 +10,32 @@ const UserSettings = () => {
     window.location = url_landing;
   }
 
+  const handleName = (e) => {
+    if(e.keyCode === 13) {  
+      user.name = e.target.value;
+      localStorage.setItem("user", JSON.stringify(user))
+    }
+  }
+
+  const handleEmail = (e) => {
+    if(e.keyCode === 13) {  
+      user.email = e.target.value;
+      localStorage.setItem("user", JSON.stringify(user))
+    }
+  }
+
   return (
     <div>
       <h1 className='settings-header'>User Settings</h1>
 
       <div className='settings-line'>
         <p className='settings-label'>Name</p>
-        <input className='settings-inputtext' type='text' value={user.name}/>
+        <input className='settings-inputtext' type='text' onKeyUp={handleName} defaultValue={user.name}/>
       </div>
 
       <div className='settings-line'>
         <p className='settings-label'>Email</p>
-        <input className='settings-inputtext' type='text' value={user.email}/>
+        <input className='settings-inputtext' type='text' onKeyUp={handleEmail} defaultValue={user.email}/>
       </div>
 
       <div className='settings-line'>
