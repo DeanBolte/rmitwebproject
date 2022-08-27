e9719a239ba3a1d0f2bf10d08703786bdaf31c20
Dean Bolte <dean.bolte7@gmail.com>
2022-08-27 21:36:16 +1000

    added some validity checks on sign up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -19,11 +19,34 @@ const Signup = ({ onSwapForm, users }) => {
   }
  
   const handleEmail = (e) => {
-    setEmail(e.target.value);
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const email = e.target.value;
+    if(email.match(emailRegex)) {
+      setEmail(email);
+      e.target.setCustomValidity("")
+    } else {
+      e.target.setCustomValidity("Not a valid email address")
+    }
   }
 
   const handlePassword = (e) => {
-    setPassword(e.target.value);
+    const passwordRegex = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/;
+    const password = e.target.value;
+    if(password.match(passwordRegex)) {
+      setPassword(password);
+      e.target.setCustomValidity("")
+    } else {
+      e.target.setCustomValidity("Password must contain 1 uppercase, 1 lowercase, 1 number and 1 special character")
+    }
+  }
+
+  const handleConfirmPassword = (e) => {
+    const confirmPassword = e.target.value;
+    if(confirmPassword.match(password)) {
+      e.target.setCustomValidity("")
+    } else {
+      e.target.setCustomValidity("Passwords do not match")
+    }
   }
 
   // Generate JSX code for error message
@@ -57,25 +80,25 @@ const Signup = ({ onSwapForm, users }) => {
         <h1 className='form-header'>Sign Up Form</h1>
         <div className="input-container">
           <label className='input-label'>Name</label>
-          <input className='input-text' placeholder= 'Name' onChange={handleName} type="text" name="uname" required />
+          <input className='input-text' placeholder= 'Please enter your full name' onChange={handleName} type="text" name="uname" required />
           {renderErrorMessage("uname")}
         </div>
         <div className="input-container">
           <label className='input-label'>Email</label>
-          <input className='input-text' placeholder= 'Email' onChange={handleEmail} type="text" name="email" required />
+          <input className='input-text' placeholder= 'Please enter a valid email address' onChange={handleEmail} type="text" name="email" required />
           {renderErrorMessage("email")}
         </div>
         <div className="input-container">
           <label className='input-label'>Password</label>
-          <input className='input-text' placeholder= 'Password' onChange={handlePassword} type="password" name="pass" required />
+          <input className='input-text' placeholder= 'Please enter a strong password' onChange={handlePassword} type="password" name="pass" required />
           {renderErrorMessage("pass")}
         </div>
         <div className="input-container">
           <label className='input-label'>Confirm Password</label>
-          <input className='input-text' placeholder= 'Confirm Password' type="password" name="pass" required />
+          <input className='input-text' placeholder= 'Please confirm your password' onChange={handleConfirmPassword} type="password" name="pass" required />
           {renderErrorMessage("pass")}
         </div>
-        <input className='input-button' type="submit" />
+        <input className='input-button' type="submit" name="submit" value="Create Account" />
         <div>
           <p className='button-large' onClick={() => swapForm()}>Already have an account? click here to sign in!</p>
         </div>
