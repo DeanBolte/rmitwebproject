7a1bdd70deab43a53685850e21a0f43854471a9b
Dean Bolte <dean.bolte7@gmail.com>
2022-08-28 11:19:21 +1000

    added a post push service

--- src/service/posts.js
+++ src/service/posts.js
@@ -1,46 +1,57 @@
-const posts = [
-    {
-      "id":"0",
-      "userId":"0",
-      "creationDate":"18/08/2022",
-      "body":"Hello World! this is a test post"
-    },
-    {
-      "id":"1",
-      "userId":"1",
-      "creationDate":"19/08/2022",
-      "body":"Hello World! this is another from a different user test post"
-    },
-    {
-      "id":"2",
-      "userId":"0",
-      "creationDate":"20/08/2022",
-      "body":"Hi, this is a final test post."
-    },
-    {
-      "id":"2",
-      "userId":"0",
-      "creationDate":"20/08/2022",
-      "body":"Hi, this is a final test post."
-    },
-    {
-      "id":"2",
-      "userId":"0",
-      "creationDate":"20/08/2022",
-      "body":"Hi, this is a final test post."
-    },
-    {
-      "id":"2",
-      "userId":"0",
-      "creationDate":"20/08/2022",
-      "body":"Hi, this is a final test post."
-    },
-];
-
 class Posts {
-    getAllPosts() {
-        return posts;
-    }
+  constructor() {
+    this.posts = [
+      {
+        "id":"0",
+        "userId":"0",
+        "creationDate":"18/08/2022",
+        "body":"Hello World! this is a test post"
+      },
+      {
+        "id":"1",
+        "userId":"1",
+        "creationDate":"19/08/2022",
+        "body":"Hello World! this is another from a different user test post"
+      },
+      {
+        "id":"2",
+        "userId":"0",
+        "creationDate":"20/08/2022",
+        "body":"Hi, this is a final test post."
+      },
+      {
+        "id":"2",
+        "userId":"0",
+        "creationDate":"20/08/2022",
+        "body":"Hi, this is a final test post."
+      },
+    ];
+  }
+
+  // getters
+  getAllPosts() {
+    return this.posts;
+  }
+
+  // setters
+  pushPost(post) {
+    let posts = this.retrievePosts()
+    const key = Object.keys(posts)
+    posts = {...posts, key:post}
+    this.updatePosts(posts)
+  }
+
+  // temp until a proper database can be set up
+  instantiatePosts() {
+    localStorage.setItem("posts", JSON.stringify(this.posts))
+  }
+  retrievePosts() {
+    return JSON.parse(localStorage.getItem("posts"));
+  }
+  updatePosts(newPosts) {
+    this.posts = newPosts
+    this.instantiatePosts()
+  }
 }
 
 export default new Posts();
